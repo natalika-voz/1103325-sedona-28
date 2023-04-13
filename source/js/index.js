03e4f9e169d2dabf5c9e67fd5a297e5c672caaa5
natalika-voz <kochyrovanatalia@gmail.com>
2023-04-13 23:14:22 +0500

    toggle aria expanded

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,7 @@
-const mobileMenuToggleButton = document.querySelector('.toggle-menu');
-mobileMenuToggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('page--mobile-menu');
+const html = document.querySelector('.page');
+const mobileMenuToggleButton = html.querySelector('.toggle-menu');
+
+mobileMenuToggleButton?.addEventListener('click', () => {
+  html.classList.toggle('page--mobile-menu');
+  mobileMenuToggleButton.ariaExpanded = html.classList.contains('page--mobile-menu');
 });
